59b946306256671aa34b46e398c4bc195ff0d0cc
Sven Zühlsdorf <sven.zuehlsdorf@argonauten.de>
2020-04-25 20:58:02 +0200

    add grid + img

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,9 +7,9 @@
     </div>
     <div class="about">
       <div class="about__wrapper">
-        <h1 class="about__h1">Hi, you found me!</h1>
-        <br>
-        <h2 class="about__h2">I am a Junior Web Developer currently building awesome things @ <a class="intro__link" href="https://argonauten.de/">Argonauten</a> .</h2>
+        <g-image class="about__img" src="~/assets/water.jpg" width="500"/>
+        <h2 class="about__h1">A fresh start!</h2>
+        <p class="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet adipisci, vero consectetur repellendus quidem aliquam placeat, explicabo similique nostrum, assumenda saepe suscipit perspiciatis non illum quasi voluptatem accusantium a pariatur.</p>
       </div>
     </div>
   </Layout>
@@ -43,7 +43,7 @@ export default {
   }
 
   &__h1{
-    margin-top: 20px;
+    margin-top: 10px;
     color:rgba(0, 0, 0, 0);
     font-size: 2.5em;
     -webkit-text-stroke: 1px #fff;
@@ -71,30 +71,40 @@ export default {
 
 
   &__wrapper{
-    //background-color: #fed7d7;
+      display: grid;
+    grid-template-columns: 50px 200px 1fr;
+    grid-template-rows: 260px 50px 100px 1fr;
     height: 100%;
     padding: 20px;
     background-repeat: no-repeat;
     height: 100%;
-   // padding: 5% 60px;
     background-size: 100%;
-    background-image: linear-gradient(212deg, #FFA48D 3%, #6799FF 100%);
+    background-image: linear-gradient(-32deg, #FFA48D 3%, #6799FF 100%);
+  }
+
+  &__img{
+    grid-column: 1/2;
+    grid-row-start: 1;
+    max-width: 250px;
   }
 
   &__h1{
-    margin-top: 20px;
+    grid-column: 2/4;
+    grid-row-start: 2;
     color:rgba(0, 0, 0, 0);
     font-size: 2.5em;
     -webkit-text-stroke: 1px #fff;
     font-weight: 900;
   }
 
-  &__h2{
-    color:rgba(0, 0, 0, 0);
-    font-size: 2.5em;
-    -webkit-text-stroke: 1px #fff;
+  &__text{
+    grid-column: 1/4;
+    grid-row-start: 4;
+    color: white;
+    font-size: 1em;
     font-weight: 900;
   }
+
 
   &__link{
     color:rgba(0, 0, 0, 0);
